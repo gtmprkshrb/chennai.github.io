880faac5740773c8c80d4fb1f73c96bd7de294f1
Gautam Prakash <gautamp@reapbenefit.org>
2021-09-30 14:06:20 +0530

    updated lat long

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const config = {
   style: "mapbox://styles/mapbox/light-v10",
   accessToken:
     "pk.eyJ1IjoiZ3RtcHJrc2hyYiIsImEiOiJjamZ0bXBqZnMxd3E5MnduejVjdGpuN2R4In0.vvrRpEdZWNwaKUO6vmgRHw",
-  CSV: "https://docs.google.com/spreadsheets/d/1NWpae4UxuLyMKLVzD-O4QSvgoA1DUbMzcNVE6pIrBmY/gviz/tq?tqx=out:csv&sheet=Bagalkot",
-  center: [80.0685811, 13.0474876],
+  CSV: "https://docs.google.com/spreadsheets/d/1vo0Lh0x8VkqB9jlZYpPHptJiuLx9NJzoaAqAV0WpOEU/gviz/tq?tqx=out:csv&sheet=data",
+  center: [80.0689243, 13.0478223],
   zoom: 10,
   title: "Neighbourhood Dashboard",
   description:
